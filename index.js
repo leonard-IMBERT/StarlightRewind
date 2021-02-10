096ac3462ae0242367e014209b83e57c497df444
Leonard Imbert <imbert418@gmail.com>
2021-02-10 12:45:28 +0100

    [Env] Updating dependencies & setting env var

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const app = express();
 
+const STARLIGHT_REWIND_PORT = process.env.STARLIGHT_REWIND_PORT || 3000;
+
 app.use("/ressources", express.static("ressources"));
 app.use("/scripts", express.static("src"));
 
@@ -10,6 +12,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "ressources/movie.html"));
 })
 
-app.listen(3000, () => {
-  console.log("Listening on 3000");
+app.listen(STARLIGHT_REWIND_PORT, () => {
+  console.log(`Listening on ${STARLIGHT_REWIND_PORT});
 });
